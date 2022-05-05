88842b518ee78bddd3a8cd406fd202fe63302876
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-05 16:25:33 -0700

    Update index.js

--- Chm96/index.js
+++ Chm96/index.js
@@ -3,9 +3,25 @@
 Chm96: Read CHM Files on Windows96!
 */
 
+var specialString=`\u1101\u3243\u2727\u8818\u9989\u1234\u6374\u8423\u1524\u3b33\uabcd\u8383\uEeEE\u8133\u8ee8\u456d\u80ff\u1333\u6789\u0993`;
+
 function ChmFile(ui8) {
   this.ui8=ui8;
 }
+
+
+
+
+function ui8tostr(ui8){
+var len=ui8.byteLength||ui8.length;
+var str="";
+for(var i=0;i<len;i++){
+str+=String.fromCharCode(ui8[i]);
+}
+return str;
+}
+
+
 
 
 
@@ -24,17 +40,13 @@ function parseChm(rawText) {
   for(var d=0;!ended;d++){
     try{
     var i=f+d;var g=rawText[i];var cd=g.charCodeAt(0);
-    if(rawText.slice(i).indexOf('�����������������')==0||rawText.slice(i).indexOf("'/a~�<")==0){
+    if((rawText.slice(i).indexOf('�����������������')==0)||(rawText.slice(i).indexOf("'/a~�<")==0)||(rawText.slice(i).indexOf("\n"+String.fromCharCode(65533))===0)||(rawText.slice(i).indexOf("/a~")==0)){
       ended=true;
-      if(cdr!=""){
-        dirs.push(cdr);
-      }
+      if(cdr!==""){dirs.push(cdr);}
       break
     } else if(cd==1||cd==0){
       x=false
-      if(cdr!=""){
-        dirs.push(cdr);
-      }
+      if(cdr!==""){dirs.push(cdr);}
       cdr="";
     } else if(g=='/'){
       x=true;
@@ -46,12 +58,18 @@ function parseChm(rawText) {
     }
     }catch(e){ended=true;break}
   }
+  var rdl=[];
+  for(var i=0;i<dirs.length;i++){
+    if(dirs[i].indexOf("\n")<0){
+      rdl.push(dirs[i]);
+    }
+  }
   return {
-    dirs: dirs
+    dirs: rdl
   }
 }
 
 var path="C:/CHMData-"+(Math.random()*4992934)
-var ff=await w96.FS.readstr(current.boxedEnv.args[1]);
+var ff=ui8tostr(await w96.FS.readbin(current.boxedEnv.args[1]));
 alert("CHMData at "+path)
 w96.FS.writestr(path,JSON.stringify(parseChm(ff)));
