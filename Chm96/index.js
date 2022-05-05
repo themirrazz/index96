0ecb3a97ae5bcbc31c8b27cfd68d1eae16faa306
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-05 15:15:41 -0700

    Update index.js

--- Chm96/index.js
+++ Chm96/index.js
@@ -1,3 +1,4 @@
+//!wrt
 /*
 Chm96: Read CHM Files on Windows96!
 */
@@ -5,3 +6,48 @@ Chm96: Read CHM Files on Windows96!
 function ChmFile(ui8) {
   this.ui8=ui8;
 }
+
+
+
+function parseChm(rawText) {
+  var header=rawText.slice(0,38);// $38 byte header
+  var dirs=rawText.indexOf("PMGL");
+  var indx=rawText.indexOf("PMGI");
+  var f=rawText.indexOf("/");
+  if(f<0){return 0}
+  f=f+1;
+  var ended=false;
+  var dirs=[];
+  var cdr="";
+  var ecr="���!";
+  var x=true
+  for(var d=0;!ended;d++){
+    try{
+    var i=f+d;var g=rawText[i];var cd=g.charCodeAt(0);
+    if(rawText.slice(i).indexOf('\n���!')==0){
+      ended=true;
+      dirs.push(cdr);
+      break
+    } else if(cd==1||cd==0){
+      x=false
+      dirs.push(cdr);
+      cdr="";
+    } else if(g=='/'){
+      x=true;
+      cdr+="/";
+    } else {
+      if(x){
+        cdr+=g
+      }
+    }
+    }catch(e){ended=true;break}
+  }
+  return {
+    dirs: dirs
+  }
+}
+
+var path="C:/CHMData-"+(Math.random()*4992934)
+var ff=await w96.FS.readstr(current.boxedEnv.args[1]);
+alert("CHMData at "+path)
+w96.FS.writestr(path,JSON.stringify(parseChm(ff)));
